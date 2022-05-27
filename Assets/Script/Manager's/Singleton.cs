d0dbf5971a251e8e6992f878509e345fedab71a7
wtsiwon <siwon4727@gmail.com>
2022-05-27 09:56:26 +0900

    카메라, 싱글톤 수정

--- Assets/Script/Manager's/Singleton.cs
+++ Assets/Script/Manager's/Singleton.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
 {
     public static T Instance = null;
-
+    
     protected virtual void Awake()
     {
         T t;
